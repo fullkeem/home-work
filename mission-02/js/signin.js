536a22b6be175ecc9d37b4447eeef86d2d1dd834
eddiekeem <cm730712@gmail.com>
2023-11-13 23:29:03 +0900

    mission-3 시작

---
+++ mission-02/js/signin.js
@@ -0,0 +1,4 @@
+const emailInput = document.querySelectorAll("#email");
+const pwdInput = document.querySelectorAll("#pwd");
+const emailError = document.querySelectorAll(".email-error");
+const pwdError = documemt.querySelectorAll(".pwd-error");
