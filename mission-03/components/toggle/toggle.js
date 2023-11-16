9a4941a630730015db2185190e03af65f1e1fadb
eddiekeem <cm730712@gmail.com>
2023-11-17 00:49:11 +0900

    toggle ui 완성

--- mission-03/components/toggle/toggle.js
+++ mission-03/components/toggle/toggle.js
@@ -1,11 +1,1 @@
-const selectBoxs = document.querySelector(".drop-down");
-
-function toggleBox(selectLang) {
-  selectLang.classlist.toggle("active");
-}
-
-selectBoxs.forEach((selectBox) => {
-  selectBox.addEventLsitener("click", function (e) {
-    toggleBox(selectBox);
-  });
-});
+const selectBoxs = document.querySelector(".select-lang");
