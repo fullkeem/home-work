b76c5e13e1c0b7dc81c8939d7f437ff1fc1dd84e
eddiekeem <cm730712@gmail.com>
2023-11-21 00:14:12 +0900

    기존 언어 변경 박스 select으로 변경

---
+++ mission-03/components/form/form.js
@@ -0,0 +1,5 @@
+const emailForm = document.querySelectorAll(".email-form");
+const emailLabel = document.querySelectorAll(".email-label");
+const emailInput = document.querySelectorAll(".email-input");
+const errorMsg = document.querySelectorAll(".error-message");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
